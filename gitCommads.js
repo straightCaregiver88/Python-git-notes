1c25ba6dba372a2a8d8069718607f8c2b3021bdf
Kali <straightcaregiver88@gmail.com>
2021-03-14 23:18:46 -0500

    Adding ssh comments/testing ssh conection

--- gitCommads.js
+++ gitCommads.js
@@ -1,9 +1,17 @@
 //git commit
 //git add
 //git status 
+//git config --global user.email "straightcaregiver88@gmail.com" 
 //git reset come back to a log showed in the git log
 //git log show the commit inforamtion 'versions'
 //git show show the differences
 //git branch show the esxisting branches if param create a new one
 //git checkout move trought brances !important befor moving we sould stage the changes otherwise we lose them
 //git merge  //we usually run this one on the master branch to get sub brances data to master git merge child branc
+//git remote add origin https://github.com/straightCaregiver88/Python-git-notes.git
+//git push origin master 
+//git pull origin main --allow-unrelated-histories   
+//ssh stuff
+// -ssh-keygen -t rsa -b 4096 -C "straightcaregiver88@gmail.com"
+// -eval $(ssh-agent -s)
+// -ssh-add /home/kali/.ssh/id_rsa
