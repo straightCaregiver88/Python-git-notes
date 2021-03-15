648d33f9d1ceeb270d678127d2b48f274c91e53f
Kali <Kali@gmail.com>
2021-03-14 21:36:22 -0500

    Adding commit on top of branch

---
+++ gitCommads.js
@@ -0,0 +1,8 @@
+//git commit
+//git add
+//git status 
+//git reset come back to a log showed in the git log
+//git log show the commit inforamtion 'versions'
+//git show show the differences
+//git branch show the esxisting branches if param create a new one
+// test
